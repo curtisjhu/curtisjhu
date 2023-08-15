256fdcadac31c66738d5ffd0f5102fcef485d2fa
Curtis Hu <curtisjhu@berkeley.edu>
2023-08-14 23:59:05 -0700

    2d very good progress

--- pages/2d/index.js
+++ pages/2d/index.js
@@ -1,73 +1,246 @@
 const regl = require("regl")();
 const { Pane } = require("tweakpane");
 const { create, all } = require("mathjs");
+const camera = require("regl-camera")(regl, {
+    zoomSpeed: 0.6,
+    rotationSpeed: 0.6,
+    theta: Math.PI / 2,
+    distance: 3,
+});
+const d3 = require("../d3.min.js");
 
 const PARAMS = {
-  "f(x)=": 'x*x',
+    u: {
+        domain: { x: -1, y: 1 },
+        range: { x: -1, y: 1 },
+    },
+    v: {
+        domain: { x: -1, y: 1 },
+        range: { x: -1, y: 1 },
+    },
+    t: {
+        domain: { x: -1, y: 1 },
+        range: { x: -1, y: 1 },
+    },
+    x: "",
+    y: "",
+    z: "",
+	axis: {
+		xAxis: { x: -1, y: 1 },
+		yAxis: { x: -1, y: 1 },
+		zAxis: { x: -1, y: 1 },
+	},
+
+    numPoints: 1000,
 };
 
 const pane = new Pane({
-	title: 'Graphing',
-	expanded: true,
+    title: "Parametric Graphing",
+    expanded: true,
 });
-pane.addInput(PARAMS, 'f(x)=');
+pane.addInput(PARAMS, "numPoints", { format: (e) => e.toFixed(0) });
 
-async function getPoints() {
-	const math = create(all);
-	const node = math.parse(PARAMS["f(x)="]); // into node tree
-	const code = node.compile(); // compile node tree
+const func = pane.addFolder({
+    title: "Functions",
+    expanded: true,
+});
+func.addInput(PARAMS, "x");
+func.addInput(PARAMS, "y");
+func.addInput(PARAMS, "z");
 
-	var domain = [-1, 1];
-	var range = [-1, 1];
+const sub = pane.addFolder({
+    title: "Domain/Range",
+    expanded: false,
+});
 
-	const num = 100;
-	var coords = [];
-	for (var i = 0; i < num; i++) {
-		var x = (domain[1]-domain[0])*(i/num) + domain[0];
-		var y = code.evaluate({ x: x});
-		coords[i] = [x, (range[1]-range[0])*y + range[0]]
-	}
-	return getPoints();
+const x = sub.addFolder({
+    title: "axis",
+    expanded: true,
+});
+x.addInput(PARAMS["axis"], "xAxis");
+x.addInput(PARAMS["axis"], "yAxis");
+x.addInput(PARAMS["axis"], "zAxis");
+
+const t = sub.addFolder({
+    title: "t",
+    expanded: false,
+});
+t.addInput(PARAMS["t"], "domain");
+t.addInput(PARAMS["t"], "range");
+
+const u = sub.addFolder({
+    title: "u",
+    expanded: false,
+});
+u.addInput(PARAMS["u"], "domain");
+u.addInput(PARAMS["u"], "range");
+
+const v = sub.addFolder({
+    title: "v",
+    expanded: false,
+});
+v.addInput(PARAMS["v"], "domain");
+v.addInput(PARAMS["v"], "range");
+
+const examples = pane.addFolder({
+    title: "Examples",
+    expanded: false,
+});
+
+// Creating points
+
+function getPointsSingleParametric() {
+    var tDom = PARAMS.t.domain;
+    var t = tDom.x,
+        dt = (tDom.y - tDom.x) / PARAMS["numPoints"];
+    const coords = d3.range(PARAMS["numPoints"]).map((el, ind) => {
+        t += dt;
+        return {
+            x: Math.pow(Math.cos(t), 3),
+            y: Math.pow(Math.sin(t), 3),
+            z: 0,
+            color: [0, 0.7, 0],
+        };
+    });
+
+    return coords;
 }
 
-var coords = [[-1, -1], [1, 1], [-1, 1], [1, -1]];
-// getPoints().then((e) => coords = e);
-
+var coords = getPointsSingleParametric();
 
 const draw = regl({
     uniforms: {
-		background: [1, 1, 1],
-		axisColor: [0, 0, 0],
-		functionColor: [1, 0, 0],
-		lineWidth: 0.008,
-		func: regl.prop("func")
+        background: [1, 1, 1],
+        pointWidth: 3,
+        func: regl.prop("func"),
     },
     attributes: {
-        position: coords,
+        position: regl.prop("coords"),
+        colors: regl.prop("colors"),
     },
-    count: coords.length,
-	primitive: "line",
+    count: regl.prop("numPoints"),
+    primitive: "line strip",
+    lineWidth: 1,
     vert: `
     precision mediump float;
-    attribute vec2 position;
-	varying vec2 uv;
+    attribute vec3 position;
+    attribute vec3 colors;
+	uniform float pointWidth;
+	varying vec3 fragColor;
+	uniform mat4 projection, view;
+
     void main() {
-		uv = position;
-    	gl_Position = vec4(position, 0, 1);
+		gl_PointSize = pointWidth;
+		fragColor = colors;
+    	gl_Position = projection*view*vec4(position, 1);
     }`,
     frag: `
     precision mediump float;
-	varying vec2 uv;
-	uniform vec3 background;
+	varying vec3 fragColor;
 
     void main() {
-
-		vec3 col = background;
-      	gl_FragColor = vec4(col, 1);
+      	gl_FragColor = vec4(fragColor, 1);
     }`,
+});
 
+const drawPlane = regl({
+    uniforms: {
+        color: [0.3, 0.3, 0.3, 0.4],
+    },
+    attributes: {
+        position: regl.prop("domain"),
+    },
+    count: 6,
+    vert: `
+    precision mediump float;
+    attribute vec3 position;
+	uniform mat4 projection, view;
+
+    void main() {
+    	gl_Position = projection*view*vec4(position, 1);
+    }`,
+    frag: `
+    precision mediump float;
+	uniform vec4 color;
+
+    void main() {
+      	gl_FragColor = color;
+    }`,
+});
+
+const drawAxis = regl({
+    uniforms: {
+        color: [0.2, 0.2, 0.2, 0.4],
+    },
+    attributes: {
+        position: regl.prop("domain"),
+    },
+    count: 2,
+    primitive: "line strip",
+    vert: `
+    precision mediump float;
+    attribute vec3 position;
+	uniform mat4 projection, view;
+
+    void main() {
+    	gl_Position = projection*view*vec4(position, 1);
+    }`,
+    frag: `
+    precision mediump float;
+	uniform vec4 color;
+
+    void main() {
+      	gl_FragColor = color;
+    }`,
 });
 
 regl.frame(({ time }) => {
-    draw();
+    regl.clear({
+        color: [1, 1, 1, 1],
+        depth: 1,
+    });
+
+    camera((state) => {
+        draw({
+            coords: coords.map((e) => [e.x, e.y, e.z]),
+            colors: coords.map((e) => e.color),
+            numPoints: coords.length,
+        });
+        drawPlane({
+            domain: [
+                [-1, 0, -1],
+                [-1, 0, 1],
+                [1, 0, -1],
+                [-1, 0, 1],
+                [1, 0, -1],
+                [1, 0, 1],
+            ],
+        });
+
+        // Draw the Axis
+
+        // x
+        drawAxis({
+            domain: [
+                [-1, 0, 0],
+                [1, 0, 0],
+            ],
+        });
+
+        // y
+        drawAxis({
+            domain: [
+                [0, -1, 0],
+                [0, 1, 0],
+            ],
+        });
+
+        // z
+        drawAxis({
+            domain: [
+                [0, 0, -1],
+                [0, 0, 1],
+            ],
+        });
+    });
 });
---
+++ pages/2d/readme.md
@@ -0,0 +1,3 @@
+## Creating Fake Transparency
+
+https://observablehq.com/@rreusser/faking-transparency-for-3d-surfaces
