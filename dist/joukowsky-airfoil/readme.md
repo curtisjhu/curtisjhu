8e106a71db9ad5672ab477bad4a8afdff506b8af
Curtis Hu <curtisjhu@berkeley.edu>
2023-10-21 11:40:05 -0700

    joukowsky airfoil version 1

---
+++ dist/joukowsky-airfoil/readme.md
@@ -0,0 +1,5 @@
+TODO:
+- add vector flow
+- email this guy
+j.ponce@uq.edu.au
+(complex analysis book)
---
+++ pages/joukowsky-airfoil/index.js
@@ -0,0 +1,188 @@
+const regl = require("regl")({
+	extensions: ["oes_standard_derivatives"]
+});
+const {cfuncs} = require("./cfuncs.js")
+const mouseWheel = require("mouse-wheel");
+const mouseMove = require("mouse-change");
+
+const { Pane } = require("tweakpane");
+const TweakpaneLatex = require("tweakpane-latex");
+
+
+const PARAMS = {
+	transform: true,
+	offsetX: -0.1,
+	offsetY: 0.08
+}
+const pane = new Pane({
+    title: "Joukowsky's Airfoil (Prototype I)"
+})
+pane.registerPlugin(TweakpaneLatex);
+pane.addBlade({
+  view: "latex",
+  content: `
+# The Joukowsky's Airfoil
+This famous airfoil is a very famous complex function.
+Read more here.
+<a href="https://complex-analysis.com/content/joukowsky_airfoil.html">Great resource here</a>
+<a href="https://rreusser.github.io/joukowsky-airfoil/">Inspired by rreusser</a>
+
+`,
+  border: false,
+  markdown: true,
+});
+
+pane.addInput(PARAMS, "transform");
+pane.addInput(PARAMS, "offsetX");
+pane.addInput(PARAMS, "offsetY");
+
+
+//  Function from Iñigo Quiles
+//  https://www.shadertoy.com/view/MsS3Wc
+// vec3 hsb2rgb( in vec3 c ){
+//     vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+//                              6.0)-3.0)-1.0,
+//                      0.0,
+//                      1.0 );
+//     rgb = rgb*rgb*(3.0-2.0*rgb);
+//     return c.z * mix(vec3(1.0), rgb, c.y);
+// }
+
+//let JkTransX = (x,y) => rd*x-0.15 + (rd*x-0.15)/((rd*x-0.15)*(rd*x-0.15)+(rd*y+0.23)*(rd*y+0.23));
+//let JkTransY = (x,y) => rd*y+0.23 - (rd*y+0.23)/((rd*x-0.15)*(rd*x-0.15)+(rd*y+0.23)*(rd*y+0.23));
+const draw = regl({
+    uniforms: {
+        u_resolution: (ctx) => {
+			if (ctx.viewportWidth > ctx.viewportHeight) {
+				return [ctx.viewportHeight/ctx.viewportWidth,1]
+			}
+            return [1, ctx.viewportWidth/ctx.viewportHeight]
+        },
+		gridWidth: 1,
+		opacity: 0.9,
+		pixelRatio: (ctx) => ctx.pixelRatio,
+		t: (ctx) => ctx.time,
+		scale: regl.prop("scale"),
+		gridSpacing: 2,
+		offsetX: regl.prop("offsetX"),
+		offsetY: regl.prop("offsetY"),
+		transform: regl.prop("transform"),
+    },
+    attributes: {
+        position: [
+            [-1, -1],
+            [1, 1],
+            [-1, 1],
+            [1, 1],
+            [-1, -1],
+            [1, -1],
+		],
+
+	},
+    count: 6,
+    vert: `
+    precision highp float;
+    attribute vec2 position;
+	varying vec2 uv;
+
+    void main() {
+		uv = position;
+    	gl_Position = vec4(uv, 0, 1);
+    }`,
+    frag: `
+    #extension GL_OES_standard_derivatives : enable
+  	#define PI 3.141592653589793238
+  	#define TO_RADIANS 0.01745329251
+  	precision mediump float;
+	varying vec2 uv;
+	uniform vec2 u_resolution;
+	uniform float pixelRatio, gridWidth, opacity, t, gridSpacing, scale;
+	uniform float offsetX, offsetY;
+	uniform bool transform;
+
+	${cfuncs}
+
+	vec3 hsl2rgb( in vec3 c ){
+		vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0,1.0);
+		return c.z + c.y * (rgb-0.5)*(1.0-abs(2.0*c.z-1.0));
+	}
+
+	float gridFactor (vec2 parameter, float width, float feather) {
+      float w1 = width - feather * 0.5;
+      vec2 d = fwidth(parameter);
+      vec2 looped = 0.5 - abs(mod(parameter, 1.0) - 0.5);
+      vec2 a2 = smoothstep(d * w1, d * (w1 + feather), looped);
+      return min(a2.x, a2.y);
+    }
+
+	vec2 f(vec2 z) {
+		// f(z) = z + 1/z;
+		// f^-1 = ...
+
+		float dir = 1.0;
+		return (z + dir*csqrt(cmul(z,z)-4.0)) / 2.0;
+	}
+
+    void main() {
+		vec2 z = (uv.xy) * scale / u_resolution.xy;
+		vec2 rect = z;
+		
+
+		// insert function
+		if (transform) {
+			z = f(z);
+		}
+
+		// to hue
+		vec2 polar = cpolar(z).gr;
+
+		float L = 1.0 - pow(0.5, polar.r);
+		float H = polar.g / (2.0 * PI);
+
+		// H hue, 1.0 saturation, L lightness
+		vec3 hsl = vec3(H, 1.0, L);
+		vec3 col = hsl2rgb(hsl);
+
+		// apply polar gridlines
+		polar.g = polar.g * 4.0 / (PI);
+		polar.r = polar.r*gridSpacing;
+		float gridFact = gridFactor(polar, 0.4 * gridWidth * pixelRatio, 1.0);
+		col = mix(vec3(0.6), col, opacity * gridFact);
+
+		vec2 zz = z;
+		// apply circle
+		vec2 offset = vec2(offsetX, offsetY);
+		float radius = distance(zz, offset);
+
+		float w = 0.1;
+		float d = length(offset + vec2(-1.0, 0.0));
+		float isCircle = 1.0 - smoothstep(d-w, d, radius)*smoothstep(d, d-w, radius);
+		col = mix(vec3(0.0), col, isCircle);
+
+      	gl_FragColor = vec4(col, 1);
+    }`,
+});
+
+var s = 1.0;
+var gridSpacing = 2;
+var lastTimeWheel = 0;
+var lastTimeMove = 0;
+var lastPosition = { x: 0, y: 0};
+
+var offsets = { x: 0, y: 0};
+regl.frame(({ time }) => {
+    regl.clear({
+        color: [0, 0, 0, 1],
+        depth: 1,
+    });
+
+    draw({
+		scale: 3.0,
+		transform: PARAMS.transform,
+		offsetX: PARAMS.offsetX,
+		offsetY: PARAMS.offsetY
+	});
+});
+
+
+
