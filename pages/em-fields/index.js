888ee6df1c1d6f2ab5200da4caa951e2d74bf820
Curtis Hu <curtis.hu688@gmail.com>
2024-03-23 09:48:50 -0700

    templates

---
+++ pages/em-fields/index.js
@@ -0,0 +1,44 @@
+const regl = require("regl")();
+
+const draw = regl({
+    uniforms: {
+        iTime: (ctx) => ctx.time,
+    },
+    attributes: {
+        position: regl.buffer([
+            [-1, -1],
+            [1, 1],
+            [-1, 1],
+            [1, 1],
+            [-1, -1],
+            [1, -1],
+        ]),
+    },
+    count: 6,
+    vert: `
+    precision mediump float;
+    attribute vec2 position;
+	varying vec2 uv;
+    void main() {
+		uv = position;
+    	gl_Position = vec4(position, 0, 1);
+    }`,
+    frag: `
+    precision mediump float;
+	varying vec2 uv;
+	uniform float iTime;
+    void main() {
+
+		vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+      	gl_FragColor = vec4(col, 1);
+    }`,
+});
+
+regl.frame(({ time }) => {
+    regl.clear({
+        color: [0, 0, 0, 1],
+        depth: 1,
+    });
+
+    draw();
+});
--- pages/quantum-corral/readme.md
+++
@@ -1,5 +0,0 @@
-
-Julian Voss Andreae I'm a fool to try to represent quantum phenomena tangibly. 
-
-Quantum mirages - 
-
