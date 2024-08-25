b30e4436db22bedc1d888f32083015429ee7fd69
Curtis Hu <curtis.hu688@gmail.com>
2024-08-25 00:08:32 -0700

    clouds updated

--- pages/clouds/index.js
+++ pages/clouds/index.js
@@ -1,6 +1,30 @@
 const { utils } = require("./utils");
 const regl = require("regl")();
 
+const { Pane } = require("tweakpane");
+const TweakpaneLatex = require("tweakpane-latex")
+
+const pane = new Pane({
+    title: "WARNING: GPU INTENSIVE"
+})
+pane.registerPlugin(TweakpaneLatex);
+pane.addBlade({
+  view: "latex",
+  content: `
+# Volumetric Clouds
+
+Usually taking derivatives in three dimensions require 4 to 6 samples of the volume to find a normal vector.
+
+Recall directional gradients. Basically, the gradient projected on a unit vector of choice:
+$$ \\nabla_v f(x) = \\nabla f(x) \\cdot \\frac{\\textbf{v}}{|v|}$$
+
+This allows us to find the gradient only sampling twice (at current position and current position with a small step towards the light).
+
+Based on the [iquilez blog post](https://iquilezles.org/articles/derivative/)
+`,
+  border: false,
+  markdown: true,
+});
 
 const draw = regl({
     uniforms: {
@@ -38,40 +62,70 @@ const draw = regl({
 
     ${utils}
 
+    float map(in vec3 p) {
+        return fbm(p);
+    }
+
+    const vec3 sundir = vec3(0.30, 0.0, 1.0);
     void main() {
 
-        // sky color
-		vec4 col = vec4(135./255., 206./255., 235./255., 1.0);
+        vec2 st = uv;
+        st.y = st.y*propRatio;
 
-        float t = iTime * 0.01;
+        float cameraDist = 5.0;
+        vec3 ro = vec3(0.0, 0.0, -cameraDist);
+        vec3 rd = normalize(vec3(st, 1.0));
 
-        vec2 p = uv;
-        p.y = uv.y*propRatio;
-        p += vec2(t, 0.);
+        // background
+        vec3 col = vec3(0.45,0.5,0.65) - rd.y*0.2*vec3(0.8,0.8,1.0) + 0.15*0.55;
 
-
-        vec3 sun = vec3(1.0, 1.0, .3);
-        vec3 ro = vec3(0.0, 0.0, -5.);
-        vec3 rd = normalize(vec3(uv, 1.0));
-
-        // ro.z + rd.z * t = 0
-        // ro.x + rd.x * t = x_pl
-        // ro.y + rd.y * t = y_pl
-        // x_pl^2 + y_pl^2 < r^2
-        float x = ro.x + rd.x * (-ro.z/rd.z);
-        float y = ro.y + rd.y * (-ro.z/rd.z);
-
-        float r = distance(vec2(x,y), sun.xy);
-
-
-        // sun first
-        col = mix(vec4(245./255., 226./255., 79./255., .7), col, smoothstep(0.1, 1.8, r - sun.b));
-        col = mix(vec4(0.0), col, smoothstep(0.0, 0.1, r - sun.b));
+        // sun
+        float sun = clamp(dot(sundir, rd), 0.0, 1.0);
+        col += 0.3*vec3(1.0,0.6,0.1)*pow( sun, 8.0 ); 
 
         // clouds
-        col = mix(vec4(0.8, 0.8, 0.8, 0.8), col, fbm(p));
+        float cloudTop = -0.5;
+        float cloudBot = -3.0;
+        float tTop = (cloudTop-ro.y)/rd.y;
+        float tBot = (cloudBot-ro.y)/rd.y;
         
-      	gl_FragColor = col;
+        if (tTop < 0.0 || tTop > 20.0) {
+      	    gl_FragColor = vec4(col, 1.0);
+            return;
+        }
+
+        // clouds
+        float eps = 0.2;
+        vec4 sum = vec4(0.0);
+        const float dt = 0.02;
+        float t = tTop;
+        for (int i = 0; i < 60; i++) {
+            vec3 p = ro + rd*t;
+            float den = map(p, 5);
+
+            // hit cloud
+            if (den > 0.01) {
+                float dif = clamp((den - map(p+eps*sundir, 5))/eps, 0.0, 1.0 );
+                vec3  lin = vec3(0.65,0.65,0.75)*1.1 + 0.8*vec3(1.0,0.6,0.3)*dif;
+                vec4  col = vec4( mix( vec3(1.0,0.93,0.84), vec3(0.25,0.3,0.4), den ), den );
+                col.xyz *= lin;
+
+                // fog
+                col.xyz = mix(col.xyz, vec3(0.3), 1.0-exp2(-0.1*t));
+
+                // composite front to back
+                col.w    = min(col.w*8.0*dt,1.0);
+                col.rgb *= col.a;
+                sum += col*(1.0-sum.a);
+            }
+            t += dt;
+            if (t > tBot || sum.a > 0.99) break;
+        }
+        
+        sum = clamp(sum, 0.0, 1.0);
+        
+        col = col * (1.0 - sum.w) + sum.rgb;
+      	gl_FragColor = vec4(col, 1.0);
     }`,
 });
 
--- pages/clouds/utils.js
+++ pages/clouds/utils.js
@@ -1,8 +1,8 @@
 exports.utils = `
 
-float rand (in vec2 st) {
-	return fract(sin(dot(st.xy,
-						vec2(12.9898 + SEED, 78.233 + SEED)))
+float rand (in vec3 st) {
+	return fract(sin(dot(st,
+						vec3(12.9898 + SEED, 78.233 + SEED, 43.103 + SEED)))
 				* 43758.5453123 * SEED);
 }
 
@@ -11,39 +11,71 @@ float rand (in float x) {
 				* 43758.5453123);
 }
 
-float noise(in vec2 pos) {
-	vec2 i = floor(pos);
-	vec2 f = fract(pos);
+// Stolen from here:
+// https://www.shadertoy.com/view/4dS3Wd
+#define NOISE fbm
+#define NUM_NOISE_OCTAVES 5
+// Precision-adjusted variations of https://www.shadertoy.com/view/4djSRW
+float hash(float p) { p = fract(p * 0.011); p *= p + 7.5; p *= p + p; return fract(p); }
+float hash(vec2 p) {vec3 p3 = fract(vec3(p.xyx) * 0.13); p3 += dot(p3, p3.yzx + 3.333); return fract((p3.x + p3.y) * p3.z); }
+float noise(float x) {
+    float i = floor(x);
+    float f = fract(x);
+    float u = f * f * (3.0 - 2.0 * f);
+    return mix(hash(i), hash(i + 1.0), u);
+}
+float noise(vec3 x) {
+    const vec3 step = vec3(110, 241, 171);
 
-	float a = rand(i);
-	float b = rand(i + vec2(1.0, 0.0));
-	float c = rand(i + vec2(0.0, 1.0));
-	float d = rand(i + vec2(1.0, 1.0));
+    vec3 i = floor(x);
+    vec3 f = fract(x);
+ 
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the 
+    // incremental change to the 1D based on the 3D -> 1D wrapping
+    float n = dot(i, step);
 
-	vec2 u = smoothstep(0.,1.,f);
-
-	return mix(a, b, u.x) +
-		(c - a)* u.y * (1.0 - u.x) +
-		(d - b) * u.x * u.y;
+    vec3 u = f * f * (3.0 - 2.0 * f);
+    return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y),
+               mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
 }
 
-
-float fbm( vec2 p )
+float fbm( in vec3 p )
 {
 	float res = 0.0;
 	float a = 1.;
-	float f = 11.0;
+	float f = 1.0;
 	float aTot = 0.0;
-
-	mat2 m = mat2( rand(SEED), rand(SEED + 100.),
-                    rand(SEED + 200.),  rand(SEED + 300.) );
-	for (int i = 0; i < 10; i++) {
+	vec3 shift = vec3(100.);
+	for (int i = 0; i < 5; i++) {
 		res += a * noise(f*p);
 		aTot += a;
 		a *= 0.5;
 		f *= 1.2;
-		p = m*p;
+		p = p*2.0 + shift; // random
 	}
 	return res / aTot;
+}
+
+float map( in vec3 p, int oct )
+{
+	vec3 q = p - vec3(0.0,0.1,1.0)*iTime;
+    float g = 0.5+0.5*noise( q*0.3 );
+    
+	float f;
+    f  = 0.50000*noise( q ); q = q*2.02;
+    if( oct>=2 ) 
+    f += 0.25000*noise( q ); q = q*2.23;
+    if( oct>=3 )
+    f += 0.12500*noise( q ); q = q*2.41;
+    if( oct>=4 )
+    f += 0.06250*noise( q ); q = q*2.62;
+    if( oct>=5 )
+    f += 0.03125*noise( q ); 
+    
+    f = mix( f*0.1-0.5, f, g*g );
+        
+    return 1.5*f - 0.5 - p.y;
 }
 `;
